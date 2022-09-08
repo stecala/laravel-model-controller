ca205116c41d08c3aa4c91e3f1dbd757da72dcae
stecala <ste.cala123@gmail.com>
2022-09-08 14:48:36 +0200

    build:

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('title', 'Home')
+
+@section('home')
---
+++ resources/views/import/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="container-lg">
+        <div class="row align-items-center">
+            <div class="col-3 logo">
+                Movie
+            </div>
+            <div class="col-9">
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        Movies
+                    </li>
+                    <li>
+                        Contacts
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header> 
